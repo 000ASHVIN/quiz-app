ad1b79c05ef1a5d119a003d152351762cc483dd7
ashvin solanki <nitensolanki02@gmail.com>
2021-08-13 11:49:02 +0530

    app.js changes

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false;
-
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.config.productionTip = false;
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
